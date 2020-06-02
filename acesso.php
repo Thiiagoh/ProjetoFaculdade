4574e892d2263b627298c180bcd725aff5232d34
Thiago <Thiago@DESKTOP-3NNT5U6>
2020-06-02 16:25:35 -0300

    Adicionando Template

---
+++ acesso.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Acessando o sistema!</title>
+	<meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+        <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+        <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+        <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+        <link rel="stylesheet" type="text/css" href="css/util.css">
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <?php 
+            session_start();
+            if((!isset ($_SESSION['email']) == true) and (!isset ($_SESSION['senha']) == true)){
+                session_unset();
+                echo "<script>
+                    alert('Esta página só pode ser acessada por usuário logado');
+                    window.location.href = 'index.html';
+                    </script>";
+            }
+            $logado = $_SESSION['email'];
+        ?>
+    </head>
+    <body>
+        <div class="limiter">
+            <div class="container-login100">
+                <span class="login100-form-title">Mapa</span>
+                <div>
+                    <iframe width="600" height="450" frameborder="0" style="border:0"
+                        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDESK2WzehKsr3PBY72y6DhgFzTscYEj6I&q=Brasilia+DF" allowfullscreen>
+                    </iframe>
+                  <a href="desconectar.php"><button align="center" class="login100-form-btn">Desconectar</button></a>
+                </div>  
+            </div>
+	</div>
+        <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/tilt/tilt.jquery.min.js"></script>
+	<script >
+		$('.js-tilt').tilt({
+			scale: 1.1
+		})
+	</script>
+	<script src="js/main.js"></script>
+    </body>
+</html>
---
+++ fazerLigacao.php
@@ -0,0 +1,32 @@
+<?php
+    session_start();
+    
+    $login=$_GET['email'];
+    $senha=$_GET['senha'];
+
+    $local="sql10.freesqldatabase.com";
+    $usuario_BD="sql10345169";
+    $senha_BD="UAzvU32VSN";
+    $base="sql10345169";
+    
+    $tenta_conectar =  new mysqli($local,$usuario_BD,$senha_BD,$base);
+    if($tenta_conectar->connect_error === TRUE){    
+        die("Deu erro na conexão ". $tenta_conectar->connect_error);
+    }
+    
+    $tenta_achar = "SELECT * FROM clientes WHERE email='$login' AND senha='$senha'";
+    $resultado = $tenta_conectar->query($tenta_achar);
+    if ($resultado->num_rows > 0){
+        $_SESSION['email'] = $email;
+        $_SESSION['senha'] = $senha;
+        header('location:acesso.php');
+    }
+    else{
+        session_unset();
+        session_destroy();
+        echo "<script> 
+                alert('Login ou senha incorreto');
+                window.location.href = 'index.html';
+            </script>";
+    }
+?>
